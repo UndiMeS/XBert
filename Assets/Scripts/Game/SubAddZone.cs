7c35318fd8c970e871585c241ea2995957e59d09
UndiMeS <67786979+UndiMeS@users.noreply.github.com>
2021-11-26 16:09:41 +0100

    Color Switch (bug -> Numbers update too late)

---
+++ Assets/Scripts/Game/SubAddZone.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubAddZone : MonoBehaviour
+{
+
+public Color SubtractColor;
+public UnityEngine.U2D.SpriteShapeRenderer ZoneRenderer;
+public Color AdditionColor;
+    public bool Subtract;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ZoneRenderer = this.gameObject.GetComponent<UnityEngine.U2D.SpriteShapeRenderer>();
+        if(this.gameObject.name == "SubtractZone")
+        {
+            Subtract = true;
+            ZoneRenderer.color = SubtractColor;
+        }
+        else
+        {
+            Subtract = false;
+            ZoneRenderer.color = AdditionColor;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if(Subtract == true)
+        {
+            //col.gameObject.GetComponent<aXelNumber>().Subtract = true;
+            ZoneRenderer.color = SubtractColor;
+        //Debug.Log("enter");
+        }
+        if(Subtract == false)
+        {
+            //col.gameObject.GetComponent<aXelNumber>().Subtract = false;
+            ZoneRenderer.color = AdditionColor;
+        //Debug.Log("enter");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+{
+    if(col.gameObject.tag == "Numbers")
+    {
+        if(ZoneRenderer.color == SubtractColor)
+        {
+            col.gameObject.GetComponent<aXelNumber>().Subtract = true;
+            //ZoneRenderer.color = SubtractColor;
+        //Debug.Log("enter");
+        }
+        if(ZoneRenderer.color == AdditionColor)
+        {
+            col.gameObject.GetComponent<aXelNumber>().Subtract = false;
+            //ZoneRenderer.color = AdditionColor;
+        //Debug.Log("enter");
+        }
+        
+    }
+}
+
+    void OnTriggerStay2D(Collider2D col)
+{
+    if(col.gameObject.tag == "Numbers")
+    {
+        if(ZoneRenderer.color == SubtractColor)
+        {
+            col.gameObject.GetComponent<aXelNumber>().Subtract = true;
+            //ZoneRenderer.color = SubtractColor;
+        }
+        if(ZoneRenderer.color == AdditionColor)
+        {
+            col.gameObject.GetComponent<aXelNumber>().Subtract = false;
+            //ZoneRenderer.color = AdditionColor;
+        //Debug.Log("enter");
+        }
+        
+        //Debug.Log("enter");
+    }
+}
+
+    void OnTriggerExit2D(Collider2D col)
+{
+    if(col.gameObject.tag == "Numbers")
+    {
+        if(ZoneRenderer.color == SubtractColor)
+        {
+            col.gameObject.GetComponent<aXelNumber>().Subtract = false;
+            //ZoneRenderer.color = AdditionColor;
+        }
+        if(ZoneRenderer.color == AdditionColor)
+        {
+            col.gameObject.GetComponent<aXelNumber>().Subtract = true;
+            //ZoneRenderer.color = SubtractColor;
+        //Debug.Log("enter");
+        }
+        
+        //Debug.Log("enter");
+    }
+}
+}
---
+++ Assets/Scripts/Items/ColorSwitch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorSwitch : MonoBehaviour
+{
+    public GameObject[] SubAddZones;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+            SubAddZones = GameObject.FindGameObjectsWithTag("SubAddZone");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Axel")
+        {
+            
+            foreach(GameObject SubAddZone in SubAddZones)
+            {
+                SubAddZone.GetComponent<SubAddZone>().Subtract = !SubAddZone.GetComponent<SubAddZone>().Subtract;
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
