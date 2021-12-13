f2612925e7769deb31dea6a6e338bafd84b99540
UndiMeS <67786979+UndiMeS@users.noreply.github.com>
2021-12-13 16:38:04 +0100

    Main Menu update

---
+++ Assets/Scripts/Game/Manmenu/MenuButtonManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtonManager : MonoBehaviour
+{
+
+    public GameObject StartMenu;
+    public GameObject WorldOne;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToStartMenu()
+    {
+        StartMenu.SetActive(true);
+        WorldOne.SetActive(false);
+    }
+
+
+    public void ToWorldOne()
+    {
+        StartMenu.SetActive(false);
+        WorldOne.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
