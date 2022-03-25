ec44f321a9bc3f050e8efae0c404687165d53471
UndiMeS <67786979+UndiMeS@users.noreply.github.com>
2022-03-25 10:44:47 +0100

    NumberCounter & StarCounter update

--- Assets/Scripts/Game/Manmenu/StarController.cs
+++ Assets/Scripts/Game/Manmenu/StarController.cs
@@ -66,100 +66,25 @@ public class StarController : MonoBehaviour
                 gameComplete = Gamedatas[0].GameComplete;
             }
          }
-        
-
-        // if(File.Exists(Application.persistentDataPath + "/XBertDataFile.json") && Loaded == false)
-        // {
-
-        //     //Gamedatas = FileHandler.ReadFromJSON<GameData>(filename);
-            
-
-        //     Loaded = true;
-        //     datas = FileHandler.ReadFromJSON<PlayerData>(filename);
-        //     Gamedatas = FileHandler.ReadFromJSONP<GameDatas> (filename);
-
-        //     if(Gamedatas.Count > 0)
-        //     {
-                
-
-        //         oneLevelSuccess = Gamedatas[0].OneLevelSuccess;
-        //         firstThreeStars = Gamedatas[0].FirstThreeStars;
-        //         gameFinished = Gamedatas[0].GameFinished;
-        //         gameComplete = Gamedatas[0].GameComplete;
-        //     }
-
-            
-        //     //FileHandler.SaveToJSON<PlayerData> (datas, filename);
 
 
-            
-            
+         if(datas.Count > 0)
+            {
+                Solution.StarComplete = 0;
+                for(int i = 0; i < datas.Count; i++)
+                {
 
-        //     if(datas.Count == 1)
-        //     {
-        //         //FirstAchievement.SetActive(true);
-
-        //         //FileHandler.SaveToJSON<GameData> (GameData, filename);
-
-
-                
-        //     }
-
-
-        //     if(datas.Count > 0)
-        //     {
-        //         StarComplete = 0;
-        //         for(int i = 0; i < datas.Count; i++)
-        //         {
-
-        //             if(datas[i].complete == true && datas[i].shadow == false)
-        //             {
-        //                 LevelFinishedCount += 1;
-        //             }
-
-        //             StarComplete += datas[i].score;
-
-        //             if(datas[i].score == 3)
-        //             {
-        //                 ThreeStarCount += 1;
-        //             }
                     
 
-        //         }
+                    Solution.StarComplete += datas[i].score;
 
+                   
 
-        //         if(ThreeStarCount >= 1 && firstThreeStars != 2)
-        //         {
-        //             //SecondAchievement.SetActive(true);
-        //             firstThreeStars = 1;
-        //         }
-        //         if( datas.Count >= 1 && oneLevelSuccess != 2)
-        //         {
-        //             oneLevelSuccess = 1;
-        //         }
-        //         if(LevelFinishedCount == 21 && gameFinished != 2)
-        //         {
-        //             gameFinished = 1;
-        //         }
-        //         if(StarComplete == 84 && gameComplete != 2)
-        //         {
-        //             gameComplete = 1;
-        //         }
-        //     }
+                }
 
-        //     if(Gamedatas.Count > 0)
-        //     {
-        //         Gamedatas[0] = new GameDatas(oneLevelSuccess, firstThreeStars, gameFinished, gameComplete);
-        //     }
+            }
+        
 
-        //     else
-        //     {
-        //         Gamedatas.Add (new GameDatas(oneLevelSuccess, firstThreeStars, gameFinished, gameComplete));
-        //     }
-
-            
-        //     FileHandler.SaveToJSONP<PlayerData> (datas, Gamedatas, filename);
-        // }
                     
     }
 
